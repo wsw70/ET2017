e08632bdb5524d7e1eb576df150cc808e8f19528
wojtek <w@swtk.info>
2017-09-25 20:23:39 +0200

    corrected code & data for martin

--- ma.py
+++ ma.py
@@ -13,49 +13,50 @@ with open('ma.txt', 'w') as f:
             'day',
             arrow.get('06/09/2017', 'DD/MM/YYYY', tzinfo=tz.tzlocal()),
             arrow.get('06/07/2018', 'DD/MM/YYYY', tzinfo=tz.tzlocal())
-    ):        is_vacation = False
-    is_weekend = False
-    for vac in vacances:
-        if date in vac:
-            is_vacation = True
-    _, weeknr, weekday = date.isocalendar()
-    if weekday in [6, 7]:
-        is_weekend = True
-        # print(date, weeknr, is_weekend, is_vacation)
-    if not (is_vacation or is_weekend):
-        if weekday == 1:
-            s = "8"
-            e = "17"
-        if weekday == 2:
-            s = "8"
-            e = "17"
-        if weekday == 3:
-            s = "8"
-            e = "12:20"
-        if weekday == 4:
-            s = "8"
-            e = "17"
-        if weekday == 5:
-            if weeknr % 2 == 0:  # semaine A
-                s = "9"
-            else:
+    ):
+        is_vacation = False
+        is_weekend = False
+        for vac in vacances:
+            if date in vac:
+                is_vacation = True
+        _, weeknr, weekday = date.isocalendar()
+        if weekday in [6, 7]:
+            is_weekend = True
+            # print(date, weeknr, is_weekend, is_vacation)
+        if not (is_vacation or is_weekend):
+            if weekday == 1:
                 s = "8"
-            e = "16"
+                e = "17"
+            if weekday == 2:
+                s = "8"
+                e = "17"
+            if weekday == 3:
+                s = "8"
+                e = "12:20"
+            if weekday == 4:
+                s = "8"
+                e = "17"
+            if weekday == 5:
+                if weeknr % 2 == 0:  # semaine A
+                    s = "9"
+                else:
+                    s = "8"
+                e = "16"
 
-        try:
-            sh = int(s.split(':')[0])
-            smin = int(s.split(':')[1])
-        except IndexError:
-            sh = int(s)
-            smin = 0
-        s = date.replace(hour=sh, minute=smin)
+            try:
+                sh = int(s.split(':')[0])
+                smin = int(s.split(':')[1])
+            except IndexError:
+                sh = int(s)
+                smin = 0
+            s = date.replace(hour=sh, minute=smin)
 
-        try:
-            eh = int(e.split(':')[0])
-            emin = int(e.split(':')[1])
-        except IndexError:
-            eh = int(e)
-            emin = 0
-        e = date.replace(hour=eh, minute=emin)
+            try:
+                eh = int(e.split(':')[0])
+                emin = int(e.split(':')[1])
+            except IndexError:
+                eh = int(e)
+                emin = 0
+            e = date.replace(hour=eh, minute=emin)
 
-        f.write("{0} {1}\n".format(s, e))
+            f.write("{0} {1}\n".format(s, e))
